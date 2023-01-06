aa8ebe03554944c67ca92ec62d8cc8d38c6a310c
PremerX007 <39229888+PremerX007@users.noreply.github.com>
2023-01-06 16:58:08 +0700

    completed

--- lab12_1.cpp
+++ lab12_1.cpp
@@ -1,2 +1,44 @@
-// cout << "Press Enter 3 times to reveal your future.";
-// cout << "You will get A in this 261102.";
+#include <iostream>
+using namespace std;
+
+int main(){
+    cout << "Press Enter 3 times to reveal your future.";
+    cin.get();
+    cin.get();
+    cin.get();
+
+    srand(time(0));
+    int x = rand()%9;
+    string result;
+    switch (x)
+    {
+    case 0:
+        result = "A";
+        break;
+    case 1:
+        result = "B+";
+        break;
+    case 2:
+        result = "B";
+        break;
+    case 3:
+        result = "C+";
+        break;
+    case 4:
+        result = "C";
+        break;
+    case 5:
+        result = "D+";
+        break;
+    case 6:
+        result = "D";
+        break;
+    case 7:
+        result = "F";
+        break;
+    default:
+        result = "W";
+        break;
+    }
+    cout << "You will get " << result << " in this 261102.";
+}
